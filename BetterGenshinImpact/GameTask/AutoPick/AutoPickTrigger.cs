c040a3fe7341a4162da540b9ed0a31b81b4b4d9b
AutoGreat <144865652+AutoGreat@users.noreply.github.com>
2026-01-25 10:18:52 +0800

    定制-自动拾取逻辑

--- BetterGenshinImpact/GameTask/AutoPick/AutoPickTrigger.cs
+++ BetterGenshinImpact/GameTask/AutoPick/AutoPickTrigger.cs
@@ -201,6 +201,8 @@ public partial class AutoPickTrigger : ITaskTrigger
 
     //private int _fastModePickCount = 0;
 
+    private int _mouseScrollCount = 0;
+
     public void OnCapture(CaptureContent content)
     {
         while (RunnerContext.Instance.AutoPickTriggerStopCount > 0)
@@ -381,6 +383,24 @@ public partial class AutoPickTrigger : ITaskTrigger
                 Simulation.SendInput.Keyboard.KeyPress(AutoPickAssets.Instance.PickVk);
                 return;
             }
+            
+            if (_mouseScrollCount >= 0) {
+                Simulation.SendInput.Mouse.VerticalScroll(1);
+                _mouseScrollCount += 1;
+                if (_mouseScrollCount > 10) {
+                    _mouseScrollCount = -1;
+                }
+            } else {
+                Simulation.SendInput.Mouse.VerticalScroll(-1);
+                _mouseScrollCount -= 1;
+                if (_mouseScrollCount < -10) {
+                    _mouseScrollCount = 1;
+                }
+            }
+            Thread.Sleep(100);
+
+            return;
+
             speedTimer.Record("白名单判断");
             if (isExcludeIcon)
             {
